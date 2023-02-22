8bc738892d2ccdb14456a9c0bf5c8048cdf94de2
James Monty-Carbonari <52420860+machinateddesigns@users.noreply.github.com>
2023-02-22 18:33:05 -0500

    authentication added

--- API/Program.cs
+++ API/Program.cs
@@ -1,20 +1,26 @@
+using System.Text;
 using API.Data;
+using API.Extensions;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
+builder.Services.AddIdentityServices(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer(); // removed in tutorial
 builder.Services.AddSwaggerGen(); // removed in tutorial
-
-builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -27,9 +33,11 @@ if (app.Environment.IsDevelopment()) // removed in tutorial
 
 app.UseHttpsRedirection(); // removed in tutorial
 
-app.UseAuthorization(); // removed in tutorial
-
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
+//these must come after cors, but before mapcontrollers
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
